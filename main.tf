3c03aaeabb3806f8bcb0c4847cf35600432b66fd
Rye Carrigan <rcarrigan@cloudbees.com>
2020-05-04 15:07:49 -0400

    Replacing amazon-managed policies

--- main.tf
+++ main.tf
@@ -60,29 +60,25 @@ resource "aws_iam_role" "node" {
   tags = var.extra_tags
 }
 
+resource "aws_iam_policy" "eks" {
+  description = "Collects policies from required managed policy documents"
+  name_prefix = "${var.node_name_prefix}-eks"
+  policy      = data.aws_iam_policy_document.eks.json
+}
+
 resource "aws_iam_policy" "eks_autoscaling" {
   description = "EKS worker node autoscaling policy for cluster ${var.cluster_name}"
   name_prefix = "${var.node_name_prefix}-autoscaling"
   policy      = data.aws_iam_policy_document.autoscaling.json
 }
 
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = aws_iam_policy.eks.arn
+  role       = aws_iam_role.node.id
+}
+
 resource "aws_iam_role_policy_attachment" "eks_autoscaling" {
   policy_arn = aws_iam_policy.eks_autoscaling.arn
-  role       = aws_iam_role.node.id
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.id
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_node" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.id
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_read_only" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node.id
 }
 
@@ -130,16 +126,15 @@ data "aws_iam_policy_document" "autoscaling" {
   }
 
   statement {
-    sid    = "eksWorkerAutoscalingOwn"
-    effect = "Allow"
-
     actions = [
       "autoscaling:SetDesiredCapacity",
       "autoscaling:TerminateInstanceInAutoScalingGroup",
       "autoscaling:UpdateAutoScalingGroup",
     ]
 
+    effect    = "Allow"
     resources = ["*"]
+    sid       = "eksWorkerAutoscalingOwn"
 
     condition {
       test     = "StringEquals"
@@ -152,6 +147,73 @@ data "aws_iam_policy_document" "autoscaling" {
       variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
       values   = ["true"]
     }
+  }
+}
+
+data "aws_iam_policy_document" "eks" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings",
+    ]
+
+    effect    = "Allow"
+    sid       = "AmazonEC2ContainerRegistryReadOnly"
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ec2:AssignPrivateIpAddresses",
+      "ec2:AttachNetworkInterface",
+      "ec2:CreateNetworkInterface",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeInstances",
+      "ec2:DescribeTags",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeInstanceTypes",
+      "ec2:DetachNetworkInterface",
+      "ec2:ModifyNetworkInterfaceAttribute",
+      "ec2:UnassignPrivateIpAddresses",
+    ]
+
+    effect    = "Allow"
+    resources = ["*"]
+    sid       = "AmazonEKSCNIPolicy1"
+  }
+
+  statement {
+    actions   = ["ec2:CreateTags"]
+    effect    = "Allow"
+    resources = ["arn:aws:ec2:*:*:network-interface/*"]
+    sid       = "AmazonEKSCNIPolicy2"
+  }
+
+  statement {
+    actions = [
+      "ec2:DescribeInstances",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumesModifications",
+      "ec2:DescribeVpcs",
+      "eks:DescribeCluster",
+    ]
+
+    effect    = "Allow"
+    resources = ["*"]
+    sid       = "AmazonEKSWorkerNodePolicy"
   }
 }
 
